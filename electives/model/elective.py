f95a58b2f2ade4f9b7e3d081209f3675f9eaecbb
SamHadavi <32000449+SamHadavi@users.noreply.github.com>
2018-04-15 23:48:36 -0700

    3

--- electives/model/elective.py
+++ electives/model/elective.py
@@ -1,5 +1,6 @@
 class Elective:
-     def __init__(self, name='name', year='year', day='day', time='time', instructor='n/a', description='description'):
+     def __init__(self,crn='crn', name='name', year='year', day='day', time='time', instructor='n/a', description='description'):
+         self.crn = crn
          self.name = name
          self.year = year
          self.day = day
--- electives/model/student.py
+++ electives/model/student.py
@@ -1,7 +1,7 @@
 
 
 class Student:
-    def __init__(self,student_number='A00000000', name='n/a', level=None):
+    def __init__(self,student_number='student_number', name='n/a', level=None):
         self.student_number = student_number
         self.name = name
         self.level = level
