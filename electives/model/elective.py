290dff04938cbd80f4b3a8f660dcdfdfb7ed939c
SamHadavi <32000449+SamHadavi@users.noreply.github.com>
2018-04-16 00:55:44 -0700

    5

--- electives/model/elective.py
+++ electives/model/elective.py
@@ -1,9 +1,11 @@
 class Elective:
-     def __init__(self,crn='crn', name='name', year='year', day='day', time='time', instructor='n/a', description='description'):
+     def __init__(self,crn='crn', name='name', year='year', day='day', time='time', level='level',
+                  instructor='instructor', description='description'):
          self.crn = crn
          self.name = name
          self.year = year
          self.day = day
          self.time = time
+         self.level = level
          self.instructor = instructor
          self.description = description
--- electives/model/student.py
+++ electives/model/student.py
@@ -1,8 +1,32 @@
 
 
 class Student:
-    def __init__(self,student_number='student_number', name='n/a', level=None):
-        self.student_number = student_number
-        self.name = name
-        self.level = level
+    def __init__(self ,student_number='student_number', name='name', level='level'):
+        self._student_number = student_number
+        self._name = name
+        self._level = level
         self.electives = []
+
+    @property
+    def student_number(self):
+        return self._student_number
+
+    @student_number.setter
+    def student_number(self, student_number):
+        self._student_number = student_number
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def level(self):
+        return self._level
+
+    @level.setter
+    def level(self, level):
+        self._level = level
